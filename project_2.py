76697c99c72032ba2180cf1b144b01929d23dac5
Witchho <vinkler.prodej@gmail.com>
2022-01-22 22:03:22 +0100

    dokončeno generovaní náhodného čísla. Nahození funkce na kontrolu hráče a hádání kódu

--- project_2.py
+++ project_2.py
@@ -2,24 +2,39 @@ import random
 
 def nahodne_org_cisla():
     seznam = [1,2,3,4,5,6,7,8,9]
-    cislo = str(random.sample(seznam, 4))
-    #spojit seznam do jednoho stringu
-    return cislo
+    cislo_seznam = random.sample(seznam, 4)
+    s_t_r = ''
+    for cislo in cislo_seznam:
+        s_t_r += str(cislo)
+    print(s_t_r) # vymazat print, je jenom pro kontrolu
+    return s_t_r
+
+def hra_start():
+    True
+    nahodne_org_cisla()
+
+def kontrola_hrace():
+    seznam_cisel = []
+    while True:
+        zadavani = input('Zadej váš 4 místný kód')
+        if len(str(zadavani)) != 4:
+            print('Kód má obsahovat pouze 4 číslice!')
+        elif int(zadavani) == 0:
+            print('Kód nesmí začínat 0')
+        #elif not zadavani.isnumeric():
+            #print('Kód obsahuje nepovolené znaky')
+        for cisla in zadavani:
+            if cisla in seznam_cisel:
+                seznam_cisel.append(cisla)
+                print('Číslo musí být originál')
+
+    else:
+        print('vše je OK')
+        False
+
 
 
 #uvítání
-def nahodne_gener_c():
-    while True:
-        nahodne_cislo = int(random.random() * 9999)
-
-        if len(str(nahodne_cislo)) == 4:
-            for cislo in nahodne_cislo:
-                if cislo in nahodne_cislo
-            break
-    return nahodne_cislo
-
-
-
 oddelovac = 20 * '-'
 uvitani = len('Vítám tě u hry Bulls & Cows!') * '-'
 
@@ -28,24 +43,27 @@ print('Vítám tě u hry Bulls & Cows,',uvitani,
       'Tvým cílem je uhadnout tyto čísla! ',
       uvitani, sep='\n')
 
-#Náhodně generované číslo
-sifra = nahodne_gener_c()
-print(nahodne_org_cisla())
-
 #3.Hráč hádá číslo. Program jej upozorní, pokud zadá číslo kratší nebo delší než 4 čísla,
 # pokud bude obsahovat duplicity,začínat nulou, příp. obsahovat nečíselné znaky
 # Program vyhodnotí tip uživatele
 
-while True:
-    hadam = int(input('Hádej číslo:'))
-    if len(str(hadam)) != 4:
-        print('Neplatný pokus')
-    else:
-        if hadam == sifra:
-            print('Uhodnuto')
-            break
-        else:
-            print('Zkus to znovu!')
+
+
+
+
+hra_start()
+while hra_start():
+    zadavani = input('Zadej váš 4 místný kód')
+    for i,cislo in enumerate(zadavani):
+        if cislo in gener_num:
+            print(cislo)
+            print('správně')
+
+
+
+
+
+
 
 
 
